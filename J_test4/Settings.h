141494e336beae9df9ef14d68b73a124b7034c53
Deckerlsc <99098493+Deckerlsc@users.noreply.github.com>
2022-02-13 08:09:14 -0700

    Change File struc
    changed file structure for Arduino work, added power switch function in ibus_funtions

---
+++ J_test4/Settings.h
@@ -0,0 +1,15 @@
+
+// define GPIO pins for motor control
+#define RPWM0 12
+#define LPWM0 13
+#define RPWM1 18
+#define LPWM1 19
+#define REN0 22
+#define LEN0 23
+#define CMin 1000
+#define CMax 1000
+#define MMin -255
+#define MMax 255
+//#define PWM 11 // for scotts code
+#define BusArray_size 10 // change this for CC size you have 
+int CC[BusArray_size]; // define the Control Channal int array 
